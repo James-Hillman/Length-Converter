761e492d7ab581c20c1893b76890aeddecb76a63
James-Hillman <olegunner2222@gmail.com>
2019-07-12 10:10:44 +0100

    Update

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function calc() {
-  
   let input = document.querySelector("#input").value;
   let convertTo = document.querySelector("#unit").value;
   let convertFrom = document.querySelector("#unit1").value;
-  
+
   const cm = "cm";
   const mm = "mm";
   const ft = "ft";
+  const mtr = "mtr";
+  const inches = "inches";
+
   let answer;
   let end;
-  
-  
 
   //Formulas for each conversion type
-    if (convertFrom == "mm" && convertTo == "cm") {
+  if (convertFrom == "mm" && convertTo == "cm") {
     answer = input / 10;
   } else if (convertFrom == "cm" && convertTo == "mm") {
     answer = input * 10;
@@ -25,25 +25,71 @@ function calc() {
     answer = input / 304.8;
   } else if (convertFrom == "ft" && convertTo == "mm") {
     answer = input * 304.8;
-  } else if (convertFrom == "mm" && convertTo == "mm") {
-    alert ("Unable to Convert same Units");
+  } else if (convertFrom == "mtr" && convertTo == "cm") {
+    answer = input * 100;
+  } else if (convertFrom == "mtr" && convertTo == "ft") {
+    answer = input * 3.281;
+  } else if (convertFrom == "mtr" && convertTo == "mm") {
+    answer = input * 1000;
+  } else if (convertFrom == "cm" && convertTo == "mtr") {
+    answer = input / 100;
+  } else if (convertFrom == "mm" && convertTo == "mtr") {
+    answer = input / 1000;
+  } else if (convertFrom == "ft" && convertTo == "mtr") {
+    answer = input / 3.281;
+  } else if (convertFrom == "inches" && convertTo == "mtr") {
+    answer = input / 39.37;
+  } else if (convertFrom == "inches" && convertTo == "cm") {
+    answer = input * 2.54;
+  } else if (convertFrom == "inches" && convertTo == "mm") {
+    answer = input * 25.4;
+  } else if (convertFrom == "inches" && convertTo == "ft") {
+    answer = input / 12;
+  } else if (convertFrom == "cm" && convertTo == "inches") {
+    answer = input / 2.54;
+  } else if (convertFrom == "mm" && convertTo == "inches") {
+    answer = input / 25.4;  
+  } else if (convertFrom == "mtr" && convertTo == "inches") {
+    answer = input * 39.37;
+  } else if (convertFrom == "ft" && convertTo == "inches") {
+    answer = input * 12; 
+  }  
+
+  //Outputs error if the same unit is selected in both dropdowns
+    else if (convertFrom == "mm" && convertTo == "mm") {
+    alert("Unable to Convert same Units");
   } else if (convertFrom == "cm" && convertTo == "cm") {
-    alert ("Unable to Convert same Units");
+    alert("Unable to Convert same Units");
   } else if (convertFrom == "ft" && convertTo == "ft") {
-    alert ("Unable to Convert same Units");
-  } if (convertTo == "cm") {
+    alert("Unable to Convert same Units");
+  } else if (convertFrom = "mtr" && convertTo == "mtr") {
+    alert("Unable to Convert same Units");
+  } else if (convertFrom = "inches" && convertTo == "inches") {
+    alert("Unable to Convert same Units");
+  }
+
+  //Shows the result abreviation below the resulting output
+  if (convertTo == "cm") {
     end = cm;
-  } if (convertTo == "mm") {
+  }
+  if (convertTo == "mm") {
     end = mm;
-  } if (convertTo == "ft") {
+  }
+  if (convertTo == "ft") {
     end = ft;
-  } 
-  
-  console.log()
+  }
+  if (convertTo == "mtr") {
+    end = mtr;
+  }
+  if (convertTo == "inches") {
+    end = inches;
+  }
+
+  console.log();
   document.getElementById("results").innerHTML = answer.toFixed(2);
   document.getElementById("resultsAb").innerHTML = end;
-  document.getElementById("#input").reset();
-  
   
 }
-
+function refreshDiv() {
+  input.value = ""
+}
